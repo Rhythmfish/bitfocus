74e26b120c92fe01b4ed83aed6557b8f990099e7
Rhythmfish <landoperk@gmail.com>
2022-08-05 19:58:24 -0400

    updates

--- data_load.py
+++ data_load.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pathlib import Path
 import sqlalchemy
-import mysql.connector as msql
 
 # Assign directory
 directory = 'C:\\Users\\Landon\\Python Projects\\bitfocus\\files'
@@ -14,4 +13,7 @@ for file in files:
     # gets file name from full file path
     file_name = Path(file).stem
     # Insert DataFrame into db. If table exists -> replace table
-    bf_df.to_sql(file_name, con= engine,chunksize= 200, method= 'multi', if_exists= 'replace',index=False)
+        # credentials should be environmental variables regarding best practice.
+        # If engine is not passed in the loop to_sql() will timeout.
+    engine = sqlalchemy.create_engine("mysql+mysqlconnector://perkins:perkins@45.79.53.180:3306/bitfocus_perkins")
+    bf_df.to_sql(file_name, con= engine,chunksize= 1000, method= 'multi', if_exists= 'replace',index=False)
